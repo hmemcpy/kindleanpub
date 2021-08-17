257a60257b99320eaee488633bd4fc6c0a6c3d01
Igal Tabachnik <hmemcpy@gmail.com>
2021-08-17 10:55:24 +0300

    Update pdfix.sh

--- pdfix.sh
+++ pdfix.sh
@@ -12,24 +12,22 @@
 
 # Original code by Alfred Klomp, http://www.alfredklomp.com/programming/shrinkpdf/
 
-fix ()
-{
+fix() {
 	echo "Converting $IFILE > $OFILE"
 
 	# Converting using high quality, color preserving, 300 dpi imgs
 	# see https://stackoverflow.com/questions/40849325/ghostscript-pdfwrite-specify-jpeg-quality
 
-	gs									\
-		-sOutputFile="$2"				\
-		-q -dNOPAUSE -dBATCH -dSAFER	\
-		-sDEVICE=pdfwrite				\
-		-dPDFSETTINGS=/prepress			\
-		-c ".setpdfwrite << /ColorACSImageDict << /VSamples [ 1 1 1 1 ] /HSamples [ 1 1 1 1 ] /QFactor 0.08 /Blend 1 >> /ColorImageDownsampleType /Bicubic /ColorConversionStrategy /LeaveColorUnchanged >> setdistillerparams"	\
+	gs \
+		-sOutputFile="$2" \
+		-q -dNOPAUSE -dBATCH -dSAFER \
+		-sDEVICE=pdfwrite \
+		-dPDFSETTINGS=/prepress \
+		-c ".setpdfwrite << /ColorACSImageDict << /VSamples [ 1 1 1 1 ] /HSamples [ 1 1 1 1 ] /QFactor 0.08 /Blend 1 >> /ColorImageDownsampleType /Bicubic /ColorConversionStrategy /LeaveColorUnchanged >> setdistillerparams" \
 		-f "$1"
 }
 
-usage ()
-{
+usage() {
 	echo "Reduces PDF filesize by lossy recompressing images to JPEG with Ghostscript."
 	echo "Fixes rendering issues with Leanpub PDFs on older Kindle devices."
 	echo "  Usage: $1 infile [outfile]"
